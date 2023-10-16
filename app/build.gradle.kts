f64a278a09c2f6ed3a5ca8090e570a7c373cb11e
Stanislav Povedenok <stas.egor.povedenok@gmail.com>
2023-10-16 16:54:49 +0300

    dependencies update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,11 +66,11 @@ ktlint {
 dependencies {
 
     // Jetpack navigation
-    implementation("androidx.navigation:navigation-compose:2.7.1")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
 
     // Dagger Hilt
-    implementation("com.google.dagger:hilt-android:2.48")
-    kapt("com.google.dagger:hilt-android-compiler:2.48")
+    implementation("com.google.dagger:hilt-android:2.48.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.48.1")
 
     // Hilt navigation compose
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
@@ -89,23 +89,23 @@ dependencies {
     // optional - Kotlin Extensions and Coroutines support for Room
     implementation("androidx.room:room-ktx:2.5.2")
 
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0-alpha01")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0-alpha02")
 
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.09.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.2.0-alpha06")
+    implementation("androidx.compose.material3:material3:1.2.0-alpha09")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.09.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.1")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.3")
 }
 
 kapt {
